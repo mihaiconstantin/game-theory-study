cd215ce7e9ec6d4ecdeaeb60faa6a6f6fd6efdc0
mihaiconstantin <c.tinmihai@ymail.com>
2017-04-25 00:03:06 +0200

    Added IDE support via laravel-ide-helper

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Development services
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
