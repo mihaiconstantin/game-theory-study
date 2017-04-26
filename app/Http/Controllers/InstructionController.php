aa888140639733702a653f618a4d14f4a2b5550d
mihaiconstantin <c.tinmihai@ymail.com>
2017-04-26 17:25:06 +0200

    Updated InstructionController and the seeds for the instructions table

--- app/Http/Controllers/InstructionController.php
+++ app/Http/Controllers/InstructionController.php
@@ -2,46 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Instruction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class InstructionController extends Controller
 {
+
+    /**
+     * Fetches an App\Models\Instruction object from the database
+     * using the $current_url as a search phrase
+     *
+     * @param string $current_url
+     * @param string $table
+     * @return mixed
+     */
+    protected function InstructionLoader(string $current_url, string $table = 'instructions')
+    {
+        $instruction = DB::table($table)->where('current_url', $current_url)->first();
+        $instruction->url_parameters = [];
+        return $instruction;
+    }
+
+    # # #
+
     public function start()
     {
-        $instruction = DB::table('instructions')->where('current_url', 'instruction/start')->first();
-        return view('instruction', compact('instruction'));
+        return view('instruction', ['instruction' => $this->InstructionLoader('instruction.start')]);
     }
 
     public function gameOverviewOne()
     {
-        return 'gameOverviewOne';
+        return view('instruction', ['instruction' => $this->InstructionLoader('instruction.game-overview-one')]);
     }
 
     public function practice()
     {
-        return 'practice';
+        $instruction = $this->InstructionLoader('instruction.practice');
+        $instruction->url_parameters['gameNumber'] = 1;
+        $instruction->url_parameters['phaseNumber'] = 2;
+        return view('instruction', ['instruction' => $instruction]);
     }
 
     public function gameOverviewTwo()
     {
-        return 'gameOverviewTwo';
+        return view('instruction', ['instruction' => $this->InstructionLoader('instruction.game-overview-two')]);
     }
 
     public function endGame($gameNumber)
     {
-        return 'endGame' . $gameNumber;
+        $instruction = $this->InstructionLoader('instruction.end-game');
+        $instruction->url_parameters['gameNumber'] = $gameNumber;
+        return view('instruction', ['instruction' => $instruction]);
     }
 
     public function newGame($gameNumber)
     {
-        return 'newGame' . $gameNumber;
+        $instruction = $this->InstructionLoader('instruction.new-game');
+        $instruction->url_parameters['gameNumber'] = $gameNumber;
+        $instruction->url_parameters['phaseNumber'] = 1;
+        return view('instruction', ['instruction' => $instruction]);
     }
 
     public function amazonCode()
     {
-        return 'amazonCode';
+        return view('instruction', ['instruction' => $this->InstructionLoader('instruction.amazon-code')]);
     }
 
 }
--- database/seeds/InstructionTableSeeder.php
+++ database/seeds/InstructionTableSeeder.php
@@ -19,56 +19,56 @@ class InstructionTableSeeder extends Seeder
 
         // amazon-code
         Instruction::create([
-            'current_url' => 'instruction/amazon-code',
-            'next_url' => '0',
+            'current_url' => 'instruction.amazon-code',
+            'next_url' => 'instruction.start',
             'title' => 'Amazon code',
             'text' => 'Generating amazon code here and saying "bye-bye miss American Pie"'
         ]);
 
         // end-game/{gameNumber}
         Instruction::create([
-            'current_url' => 'instruction/end-game',
-            'next_url' => 'form/game-question/{gameNumber}',
+            'current_url' => 'instruction.end-game',
+            'next_url' => 'form.game-question',
             'title' => 'End of game {gameNumber}',
             'text' => 'Game {gameNumber} has just ended. The user will be redirected to "form/game-question/{gameNumber}"'
         ]);
 
         // game-overview-one
         Instruction::create([
-            'current_url' => 'instruction/game-overview-one',
-            'next_url' => 'instruction/practice',
+            'current_url' => 'instruction.game-overview-one',
+            'next_url' => 'instruction.practice',
             'title' => 'General instructions one',
             'text' => 'The user finds out that he is going to play a game and then he is redirected to "instruction/practice"'
         ]);
 
         // game-overview-two
         Instruction::create([
-            'current_url' => 'instruction/game-overview-two',
-            'next_url' => 'form/expectation',
+            'current_url' => 'instruction.game-overview-two',
+            'next_url' => 'form.expectation',
             'title' => 'General instructions two',
             'text' => 'Final instruction before the games start. Redirects to "form/expectation"'
         ]);
 
         // new-game/{gameNumber}
         Instruction::create([
-            'current_url' => 'instruction/new-game',
-            'next_url' => 'game/play/{gameNumber}/{phaseNumber}',
+            'current_url' => 'instruction.new-game',
+            'next_url' => 'game.play',
             'title' => 'New game {gameNumber}',
             'text' => 'User finds out that a new game is about to start. Redirects to "game/play/{gameNumber}/{phaseNumber}"'
         ]);
 
         // practice
         Instruction::create([
-            'current_url' => 'instruction/practice',
-            'next_url' => 'game/play/{0}/{1}',
+            'current_url' => 'instruction.practice',
+            'next_url' => 'game.play',
             'title' => 'Practice phase instructions',
             'text' => 'Practice instructions then user is redirected to "game/play/{0}/{1}'
         ]);
 
         // start
         Instruction::create([
-            'current_url' => 'instruction/start',
-            'next_url' => 'form/demographics',
+            'current_url' => 'instruction.start',
+            'next_url' => 'form.demographics',
             'title' => 'Welcome instructions',
             'text' => 'The user has landed on the welcome page. Redirects to "form/demographics'
         ]);
