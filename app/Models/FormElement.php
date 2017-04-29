b5b882f9894394a9e0839c6b170374fe0a33bb8e
mihaiconstantin <c.tinmihai@ymail.com>
2017-04-29 18:05:13 +0200

    Added static function wrapper to retrive form elements and their children give a string

--- app/Models/FormElement.php
+++ app/Models/FormElement.php
@@ -6,8 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class FormElement extends Model
 {
+    /**
+     * Defining relationship with select_options table.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function select_options()
     {
         return $this->hasMany('App\Models\SelectOption');
     }
+
+
+    /**
+     * Fetches the form elements and their children (only the cause of select elements)
+     * using the current_url as an indicator of the context.
+     *
+     * @param string $current_url
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public static function getElementForContext(string $current_url)
+    {
+        return self::with(array(
+            'select_options' => function($query) {
+                $query->orderBy('order');
+            }))
+        ->where('current_url', $current_url)
+        ->orderBy('order')
+        ->get();
+    }
+
 }
