f72082860ed17abcab74cee2d7ccfcb1bcf58609
mihaiconstantin <c.tinmihai@ymail.com>
2017-04-29 13:20:46 +0200

    Added custom query method to retrive questionnaire items and scale

--- app/Models/ItemScale.php
+++ app/Models/ItemScale.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemScale extends Model
 {
-    //
+    /**
+     * Fetches all the steps associated with a particular questionnaire
+     * (i.e., the Likert scale range)
+     *
+     * @param string $questionnaireName
+     * @return mixed
+     */
+    public static function getScaleForQuestionnaire(string $questionnaireName)
+    {
+        return self::where('name', $questionnaireName)->orderBy('order')->pluck('text')->all();
+    }
 }
--- app/Models/PersonalityItem.php
+++ app/Models/PersonalityItem.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class PersonalityItem extends Model
 {
-    //
+    /**
+     * Fetches all the items associated with a particular questionnaire.
+     *
+     * @param string $questionnaireName
+     * @return mixed
+     */
+    public static function getItemsForQuestionnaire(string $questionnaireName)
+    {
+        return self::where('name', $questionnaireName)->orderBy('order')->pluck('text')->all();
+    }
 }
