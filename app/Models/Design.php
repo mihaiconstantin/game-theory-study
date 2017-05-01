76596029ae4d99cbf8e864f44d7048d3bbcc198e
mihaiconstantin <c.tinmihai@ymail.com>
2017-05-01 14:35:19 +0200

    Updated Design and Study models with query wrappers

--- app/Models/Design.php
+++ app/Models/Design.php
@@ -6,4 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 class Design extends Model
 {
 
+    /**
+     * Fetch specific columns from designs table by name.
+     *
+     * @param string $name
+     * @param array $columns
+     * @return array
+     */
+    public static function getByName(string $name, array $columns) : array {
+        return self::where('name', $name)->select($columns)->first()->toArray();
+    }
+
 }
--- app/Models/Study.php
+++ app/Models/Study.php
@@ -6,4 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 class Study extends Model
 {
 
+    /**
+     * Fetches a study associated with a particular name.
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public static function getStudy(string $name)
+    {
+        return self::where('name', $name)->first()->toArray();
+    }
+
 }
