639444b0773f9e02b63d0f9d4d8bac971ed45311
mihaiconstantin <c.tinmihai@ymail.com>
2017-06-19 13:10:44 +0200

    Fixed typo

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -54,7 +54,7 @@ class Handler extends ExceptionHandler
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Responsep
+     * @return \Illuminate\Http\Response
      */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
